5c21b81f27d5c1c60676924f36d1898f094ae514
romth777 <roomtheater@gmail.com>
2017-11-23 06:33:03 -0800

    update

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -4,24 +4,49 @@ import pcl
 # Load Point Cloud file
 cloud = pcl.load_XYZRGB('tabletop.pcd')
 
-
 # Voxel Grid filter
-
+vox = cloud.make_voxel_grid_filter()
+LEAF_SIZE = 0.01
+vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+cloud_filtered = vox.filter()
+filename = 'voxel_downsampled.pcd'
+pcl.save(cloud_filtered, filename)
 
 # PassThrough filter
+passthrough = cloud_filtered.make_passthrough_filter()
+filter_axis = 'z'
+passthrough.set_filter_field_name(filter_axis)
+axis_min = 0.6
+axis_max = 1.1
+passthrough.set_filter_limits(axis_min, axis_max)
 
+cloud_filtered = passthrough.filter()
+filename = 'pass_through_filtered.pcd'
+pcl.save(cloud_filtered, filename)
 
 # RANSAC plane segmentation
-
+seg = cloud_filtered.make_segmenter()
+seg.set_model_type(pcl.SACMODEL_PLANE)
+seg.set_method_type(pcl.SAC_RANSAC)
+max_distance = 0.01
+seg.set_distance_threshold(max_distance)
+inliers, coefficients = seg.segment()
 
 # Extract inliers
+extracted_inliers = cloud_filtered.extract(inliers, negative=True)
 
 # Save pcd for table
 # pcl.save(cloud, filename)
+filename = 'extracted_inlier.pcd'
+pcl.save(extracted_inliers, filename)
 
 
 # Extract outliers
-
+outlier_filter = cloud_filtered.make_statistical_outlier_filter()
+outlier_filter.set_mean_k(50)
+x = 1.0
+outlier_filter.set_std_dev_mul_thresh(x)
+cloud_filtered = outlier_filter.filter()
 
 # Save pcd for tabletop objects
 
